5dbb0510b85e2af04706039a9bc0f2f6f36f8c46
HuseynliTogrul <toghrulih@code.edu.az>
2024-11-25 09:32:57 +0400

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Login.tsx";
+import { Home } from "./pages/Home.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={<Home />}
+      />
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+    </Routes>
+  </BrowserRouter>
+);
