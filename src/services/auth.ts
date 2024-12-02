02b222c793c1d0734f5340eebc83005a4ee6fa1c
HuseynliTogrul <toghrulih@code.edu.az>
2024-12-02 14:50:48 +0400

    fix: edit service folder

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,56 +1,39 @@
+import { LoginValues, AccountValues } from "./../types/auth";
 import axios, { AxiosError } from "axios";
-import { FormInstance, message } from "antd";
-import { NavigateFunction } from "react-router-dom";
-import { HandleNewAccountParams } from "../types/types";
+import { message } from "antd";
 
-export async function login(
-  values: {
-    username: string;
-    password: string;
-  },
-  navigate: NavigateFunction,
-  form: FormInstance
-) {
+export async function login(values: LoginValues): Promise<void> {
   try {
     const res = await axios.post(
       "https://sample-backend-15ml.onrender.com/api/login",
-      {
-        username: values.username.trim(),
-        password: values.password.trim()
-      }
+      values
     );
     const data = res.data;
     localStorage.setItem("currentUser", JSON.stringify(data.name));
     message.success("Login successful!");
-    navigate("/");
   } catch (e) {
     if (e instanceof AxiosError) {
       const error = e.response?.data?.message;
-      message.error(error || "Wrong username or password!");
+      message.error(error);
     } else {
       message.error("An unexpected error occurred.");
     }
+    throw new Error("Wrong username or password!");
   }
-  form.resetFields();
 }
 
-export async function signUp({
-  values,
-  navigate,
-  form
-}: HandleNewAccountParams) {
+export async function signUp(values: AccountValues): Promise<void> {
   try {
     await axios.post(
       "https://sample-backend-15ml.onrender.com/api/users",
       values
     );
     message.success("Account created successfully!");
-    navigate("/login");
-    form.resetFields();
   } catch (e) {
     if (e instanceof AxiosError) {
       const error = e.response?.data.message;
       message.error(error);
     }
+    throw new Error("Account creation failed");
   }
 }
--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,10 +1,21 @@
 import { FormInstance } from "antd";
 import { NavigateFunction } from "react-router-dom";
 
+export interface LoginValues {
+  username: string;
+  password: string;
+}
+
+export interface HandleLoginParams{
+  values:LoginValues;
+  navigate:NavigateFunction;
+  form:FormInstance;
+}
+
 export interface AccountValues {
   username: string;
-  name: string;
   password: string;
+  name: string;
   repeatPassword: string;
 }
 
